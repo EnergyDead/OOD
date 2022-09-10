167fcb470677423d85a51ce60a12bd09c2d5e95d
Ruslan <rusvas012@gmail.com>
2022-09-10 21:26:38 +0300

    lab 1 - task 3 end

--- lab1/SimUDuckFunc/SimUDuckFunc/DuckBehaviors/FlyBehaviors.cs
+++ lab1/SimUDuckFunc/SimUDuckFunc/DuckBehaviors/FlyBehaviors.cs
@@ -4,15 +4,24 @@ public class FlyBehaviors
 {
     public static Action FlyWithWings()
     {
+        var flightCounter = FlightCounter();
+        return () =>
+        {
+            Console.WriteLine($"Fly");
+            flightCounter();
+        };
+    }
+
+    public static void FlyNoWay() => Console.WriteLine("Can`t fly");
+
+    static Action FlightCounter()
+    {
         int flightsAmount = 0;
 
         return () =>
         {
             ++flightsAmount;
-            Console.WriteLine($"Fly");
             Console.WriteLine($"Flight number:{flightsAmount}");
         };
     }
-
-    public static void FlyNoWay() => Console.WriteLine("Can`t fly");
 }
